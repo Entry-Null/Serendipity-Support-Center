e758ebba1643e0a39098419fdb27267dae48fdfa
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-02-06 03:22:27 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,39 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Entry-Null/rluaenvironment/main/main.lua", true))()
+
+DISPLAY("Bypassed")
+IDENTIFICATION(
+"EntryNil",
+"2.0",
+"Bypass Anticheat"
+)
+
+local Players = game:GetService("Players")
+ 
+local function onCharacterAdded(character)
+HALT(7)
+DELETE(ClientChar.Head.Rank)
+DELETE(ClientChar["zeh leh anti xploit ys MARK 2"])
+DISPLAY("Bypassed")
+end
+ 
+local function onCharacterRemoving(character)
+ DISPLAY("CHAR DIED")
+end
+ 
+local function onPlayerAdded(player)
+    player.CharacterAdded:Connect(onCharacterAdded)
+    player.CharacterRemoving:Connect(onCharacterRemoving)
+end
+ 
+Players.PlayerAdded:Connect(onPlayerAdded)
+NASM(ClientChar, "zeh leh anti xploit ys MARK 2", true);
+DELETE(ClientChar.Head.Rank)
+DELETE(ClientChar["zeh leh anti xploit ys MARK 2"]);
+
+DELETE(Get.StarterPlayer.StarterCharacterScripts["zeh leh anti xploit ys MARK 2"]);
+while true do 
+HALT(2);
+DELETE(ClientChar.Head.Rank);
+end
+
+Client.UserID = RANGEN(39133, 93100)
